b2a604dd6543eb9619cf8c69308a8b097a1a0778
Nabeel <azarleeban@gmail.com>
2025-03-25 22:14:55 +0500

    attribute added

--- messaging-app/chat/models.py
+++ messaging-app/chat/models.py
@@ -13,6 +13,7 @@ class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    creator = "Nabeel"
 
-    def __str__(self):
+def __str__(self):
         return f"{self.user.username}: {self.content[:20]}..."
